976a7c391ea1e6e04716ed9ac769482500042027
Sabir Ali <alisabir167167@gmail.com>
2025-01-06 17:16:49 +0530

    zod implemented

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,4 +1,6 @@
 import { NextRequest,NextResponse } from "next/server";
+import schema from "../schema";
+
 export function GET(
    request:NextRequest,
    {params}:{params:{id:number}}
@@ -8,4 +10,25 @@ export function GET(
       return NextResponse.json({error:"User not found"},{status:404})
    return NextResponse.json({id:1,name:"sabir"})
 
+}
+
+export async function PUT(
+   request:NextRequest,
+   {params}:{params:{id:number}}){
+     const body= await request.json();
+     const validation=schema.safeParse(body);
+     if(!validation.success)
+      return NextResponse.json(validation.error.errors,{status:400});
+ if(params.id>10)
+   return NextResponse.json({error:"User not found"},{status:404});
+return NextResponse.json({id:1,name:"sabir"});
+}
+
+export function DELETE(
+   request:NextRequest,
+   {params}:{params:{id:number}}
+){
+   if(params.id>10)
+      return NextResponse.json({error:"user not found"},{status:404})
+   return NextResponse.json({});
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
+import schema from "./schema";
 
 export function GET(request:NextRequest){
    return NextResponse.json([
@@ -12,11 +13,12 @@ export async function POST(request: NextRequest) {
   try {
     // Parse the request body
     const body = await request.json();
+    const validation=schema.safeParse(body);
 
     // Validate the input
-    if (!body || typeof body.name !== 'string') {
+    if (!validation.success) {
       return NextResponse.json(
-        { error: 'Invalid request body. "name" is required.' },
+        validation.error.errors,
         { status: 400 }
       );
     }
