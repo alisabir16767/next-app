cda4e50a0b21755a3fe307b4bd067b6c077f11f3
Sabir Ali <alisabir167167@gmail.com>
2025-01-06 12:53:29 +0530

    lodding ui component added

--- app/users/new/page.tsx
+++ app/users/new/page.tsx
@@ -1,7 +1,26 @@
+'use client'
 import React from "react";
+import { useRouter } from "next/navigation";
 
-const NewUsersPage = () => {
-  return <div>NewUsersPage</div>;
+const NewUsersPage: React.FC = () => {
+  const router = useRouter();
+
+  const handleCreate = () => {
+    
+    console.log("User created");
+    router.push("/users");
+  };
+
+  return ( 
+    <> 
+      <button 
+        className="btn btn-primary"
+        onClick={handleCreate}
+      >
+        Create
+      </button>
+    </>
+  );
 };
 
 export default NewUsersPage;
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { Suspense } from "react";
 import PageTable from './pageTable';
+import Link from "next/link";
 
 
 interface Props{
@@ -12,7 +13,11 @@ const UsersPage = async ({searchParams:{sortOrder}}:Props) => {
    return (
       <>
          <h1 className="text-2xl font-bold mb-4">Users</h1>
+         <Link href="/users/new" className="btn btn-primary">New user</Link>
+         <Suspense fallback={<div>Loading...</div>}>
          <PageTable sortOrder={sortOrder}/>
+         </Suspense>
+        
       </>
    );
 };
